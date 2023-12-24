c27b84ddb75c7f40e9c061401a2484f080971a6f
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-24 00:22:42 +0000

    Singleton implementation of the auth manager

---
+++ AuthManager.cs
@@ -0,0 +1,20 @@
+﻿namespace COMP2001 {
+    public class AuthManager {
+
+        private static AuthManager _instance;
+
+        public AuthManager instance {
+            get {
+                if (_instance == null)
+                    _instance = new AuthManager();
+
+                return _instance;
+            }
+        }
+
+        private AuthManager() {
+            authTokens = new Dictionary<string, TokenInfo>();
+        }
+
+    }
+}
