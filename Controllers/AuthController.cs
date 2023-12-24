3251b03072a480b80a233acccb026d567f239787
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-24 00:14:12 +0000

    Basic logic for responding to the user auth endpoint

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace COMP2001.Controllers {
+
+    public struct AuthResponse {
+        public string Authorized { get; set; }
+        public string Token { get; set; }
+    }
+
+    [ApiController]
+    [Route("/user/auth")]
+    public class AuthController : Controller {
+
+        [HttpPost]
+        public async Task<string> Auth() {
+
+            StreamReader reader = new StreamReader(Request.Body);
+            string bodyString = await reader.ReadToEndAsync();
+
+            AuthenticationAPI.User bodyUser = JsonSerializer.Deserialize<AuthenticationAPI.User>(bodyString);
+
+            bool authorized = await AuthenticationAPI.AuthenticateUser(bodyUser.Email, bodyUser.Password);
+            string token = "";
+
+            AuthResponse response = new AuthResponse {
+                Authorized = authorized.ToString(),
+                Token = token
+            };
+
+            return JsonSerializer.Serialize(response); 
+
+        }
+    }
+}
