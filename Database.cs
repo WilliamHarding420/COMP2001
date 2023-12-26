dbacfd2ea8c4c5d973ae6f54a1ea7e6e39387df2
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-26 21:04:02 +0000

    Basic database connection

---
+++ Database.cs
@@ -0,0 +1,23 @@
+﻿using COMP2001.data;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace COMP2001 {
+    public class Database : DbContext {
+
+        public static string ConnectionString = "Server=localhost;" +
+            "Database=comp2001;" +
+            "User Id=SA;" +
+            "Password=DBPassword123;" +
+            "TrustServerCertificate=True;";
+
+        public Database()
+            :base() {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
