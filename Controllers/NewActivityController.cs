9918f6d436acd9c3e0fac86ba2c0b26c7c3d62b5
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-26 21:21:55 +0000

    Endpoint for adding a new activity

---
+++ Controllers/NewActivityController.cs
@@ -0,0 +1,37 @@
+﻿using COMP2001.data;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace COMP2001.Controllers {
+
+    [ApiController]
+    [Route("/activity/new")]
+    public class NewActivityController : Controller {
+
+        public struct BodyActivity {
+            public string Activity { get; set; }
+        }
+
+        [HttpPost]
+        public async Task NewActivity() {
+
+            StreamReader reader = new StreamReader(Request.Body);
+            string bodyString = await reader.ReadToEndAsync();
+
+            Database db = new();
+
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.PropertyNameCaseInsensitive = true;
+
+            BodyActivity bodyActivity = JsonSerializer.Deserialize<BodyActivity>(bodyString, options);
+
+            ActivityData activity = new ActivityData();
+            activity.Activity = bodyActivity.Activity;
+
+            await db.Activities.AddAsync(activity);
+            await db.SaveChangesAsync();
+
+        }
+
+    }
+}
