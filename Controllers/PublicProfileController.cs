34cbd2684dae65ab7adfe4346a77f0c94286e8b4
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-27 03:10:29 +0000

    Basic logic for retreiving a user's public information

---
+++ Controllers/PublicProfileController.cs
@@ -0,0 +1,42 @@
+﻿using COMP2001.data;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace COMP2001.Controllers {
+
+    [ApiController]
+    [Route("/user/public/{id}")]
+    public class PublicProfileController : Controller {
+
+        public struct PublicUser {
+
+            public string Username { get; set; }
+            public string AboutMe { get; set; }
+            public string ProfilePictureLink { get; set; }
+
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<string> Get([FromRoute] int id) {
+
+            Database db = new();
+
+            User dbUser = db.Users.FirstOrDefault(user => user.UserID == id);
+
+            if (dbUser == null)
+                return JsonSerializer.Serialize(new Dictionary<string, string> {
+                    { "error", "User ID doesn't exist." }
+                });
+
+            PublicUser user = new PublicUser();
+            user.Username = dbUser.Username;
+            user.AboutMe = dbUser.AboutMe;
+            user.ProfilePictureLink = dbUser.ProfilePictureLink;
+
+            return JsonSerializer.Serialize(user);
+
+        }
+
+    }
+}
