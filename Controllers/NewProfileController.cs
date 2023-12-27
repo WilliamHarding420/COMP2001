6b4cd2c8be098b55d3111a5d20908592b45af95c
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-27 01:16:09 +0000

    Basic logic for adding a new user with default profile to database

---
+++ Controllers/NewProfileController.cs
@@ -0,0 +1,42 @@
+﻿using COMP2001.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2001.Controllers {
+
+    [ApiController]
+    [Route("/user/new")]
+    public class NewProfileController : Controller {
+
+        public struct BodyUser {
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task NewUser([FromBody] BodyUser bodyUser) {
+
+            Database db = new();
+
+            User user = new User();
+
+            // Parameters from request body
+            user.Username = bodyUser.Username;
+            user.Email = bodyUser.Email;
+            user.Password = bodyUser.Password;
+
+            // Other default user details
+            user.Units = "Metric";
+            user.ActivityTimePreference = "Pace";
+            user.Language = "English";
+            user.Admin = "false";
+
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+
+        }
+
+    }
+}
