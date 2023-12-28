bca0a494589901e21bc34c067fc9bc64ca273ed5
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-28 20:48:41 +0000

    Basic endpoint for admin deleting an account

---
+++ Controllers/DeleteUserController.cs
@@ -0,0 +1,49 @@
+﻿using COMP2001.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace COMP2001.Controllers {
+
+    [ApiController]
+    [Route("/user/delete/{id}")]
+    public class DeleteUserController {
+
+        [HttpPost]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        public async Task<string> DeleteUser([FromRoute] int id, [FromBody] InfoAuth auth) {
+
+            AuthManager authManager = AuthManager.instance;
+            int userID = authManager.GetIDFromToken(auth.Token);
+
+            if (userID == -1)
+                return JsonSerializer.Serialize(new GenericResponse(false, "Invalid auth token."));
+
+            Database db = new();
+
+            User? dbUser = db.Users.Where(user => user.UserID == userID).FirstOrDefault();
+
+            if (dbUser == null)
+                return JsonSerializer.Serialize(new GenericResponse(false, "Invalid user."));
+
+
+            bool admin = authManager.CheckAdmin(dbUser);
+
+            if (!admin)
+                return JsonSerializer.Serialize(new GenericResponse(false, "Unauthorized."));
+
+            /*
+             * 
+             * Here the user would be sent off to be archived by another microservice / program.
+             * 
+             */
+
+            await db.Users.Where(user => user.UserID == id).ExecuteDeleteAsync();
+
+            return JsonSerializer.Serialize(new GenericResponse(true, $"User ID {id} deleted successfully."));
+
+        }
+
+    }
+}
