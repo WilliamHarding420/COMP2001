0d0e46283ca7304193c9f15d3271c596287bf1b1
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-28 03:48:44 +0000

    Basic GET endpoint for all activities and the activitie's ID

---
+++ Controllers/AllActivitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace COMP2001.Controllers {
+
+    [ApiController]
+    [Route("/activities")]
+    public class AllActivitiesController {
+
+        /// <summary>
+        /// Returns all of the activities in JSON format
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<string> Activities() {
+            return JsonSerializer.Serialize(new Database().Activities.ToArray());
+        }
+
+    }
+}
