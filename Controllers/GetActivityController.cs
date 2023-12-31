09ae323ce20b94f20c83cc9389883ea582de7178
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-31 02:06:46 +0000

    Endpoint to get an activity from its ID

---
+++ Controllers/GetActivityController.cs
@@ -0,0 +1,26 @@
+﻿using COMP2001.data;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace COMP2001.Controllers {
+
+    [ApiController]
+    [Route("/activity/{id}")]
+    public class GetActivityController {
+
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<string> GetActivity([FromRoute] int id) {
+
+            Database db = new();
+            IQueryable<ActivityData> activities = db.Activities.Where(activity => activity.ActivityID == id);
+
+            if (!activities.Any())
+                return JsonSerializer.Serialize(new GenericResponse(false, "No activity found."));
+
+            return JsonSerializer.Serialize(activities.FirstOrDefault());
+
+        }
+
+    }
+}
