ca71040f24e9154cf71d38ad9a5510c72d5df03e
WilliamHarding420 <william.harding-1@students.plymouth.ac.uk>
2023-12-31 02:18:59 +0000

    Created GenericResponse class to format all responses similarly using a generic type message

---
+++ data/GenericResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace COMP2001.data {
+    public class GenericResponse<T> {
+
+        public bool Success { get; set; }
+
+        public T Message { get; set; }
+
+        public GenericResponse(bool _Success, T _Message) {
+
+            Success = _Success;
+            Message = _Message;
+
+        }
+
+        public async Task<string> Serialize() {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static GenericResponse<string> UnauthorizedResponse = new GenericResponse<string>(false, "Unauthorized.");
+        public static GenericResponse<string> InvalidUserResponse = new GenericResponse<string>(false, "Invalid User.");
+        public static GenericResponse<string> InvalidTokenResponse = new GenericResponse<string>(false, "Invalid Auth Token.");
+
+    }
+}
